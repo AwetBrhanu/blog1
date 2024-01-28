f1cde5885bc7bb194b41af9777ef8dc5b4748483
awt <awetbrhanu122119@gmail.com>
2024-01-28 10:59:58 -0800

    requiremnts.txt

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.views import generic
 from .models import Post
+from django.shortcuts import render
+from django.http import HttpResponse
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+def PostList(request):
+    post_list = Post.objects.filter(status=1).order_by('-created_on')
+    return render(request, 'index.html', context={'post_list': post_list})
 
-class PostDetail(generic.DetailView):
+def PostDetail(request):
     model = Post
     template_name = 'post_detail.html'
+
+
+
+
